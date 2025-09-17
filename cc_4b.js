5db193cd28cf8f4564fab89742ce67ab8da91143
Colin Forbes <forbesm@usf.edu>
2025-09-17 19:36:44 -0400

    Initial commit

--- cc_4b.js
+++ cc_4b.js
@@ -1,32 +1,32 @@
 // Coding Challenge 4b
 
-let stock = [
+let products = [
     {category: "Apparel",
-     name: "Shirt",
+     name: "Shoes",
      price: 25.00,
      inventory: 18
      },
 
     {category: "Groceries",
-     name: "Cauliflower",
+     name: "Carrot",
      price: 5.50,
      inventory: 12
     },
 
     {category: "Electronics",
-     name: "Calculator Watch",
+     name: "Robot",
      price: 50.99,
      inventory: 30
     },
 
     {category: "Household",
-     name: "Chair",
+     name: "Table",
      price: 35.50,
      inventory: 40
     },
 
     {category: "Apparel",
-     name: "Socks",
+     name: "Hair Ties",
      price: 8.00,
      inventory: 8
     }
@@ -36,16 +36,7 @@ console.log("Step 2. Products Without Discount:");
 stock.forEach(obj => console.log(`${obj.category} | ${obj.name} | \$${obj.price} | ${obj.inventory}`));
 
 //Discount loop by category
-for (const product of stock) {
-  switch (key) {
-    case value:
-      
-      break;
-  
-    default:
-      break;
-  }
-}
+
 
 console.log("Step 3. Apply Discount By Category:")
 stock.forEach(obj => console.log(`${obj.category} | ${obj.name} | \$${obj.price} | ${obj.inventory}`));
@@ -64,6 +55,3 @@ stock.forEach(obj => console.log(`${obj.category} | ${obj.name} | \$${obj.price}
 
 
 //Log all product inventory, destructuring
-for (const [index, item] of stock.entries()) {
-  console.log(`${index}: ${stock.category} | ${stock.name} | \$${stock.price} | ${stock.inventory}`);
-}
