11c7dac5d101b3770d9cc3ced82351cafb632344
thecodingdilettante <forbesm@usf.edu>
2025-09-24 12:19:35 -0400

    Step 2 done

--- cc_5.js
+++ cc_5.js
@@ -1,42 +1,9 @@
-// Coding Challenge 4b
+// Coding Challenge 5
 
 let employees = [
-    {sku: "001",
-     category: "apparel",
-     name: "Shoes",
-     price: 25.00,
-     inventory: 18
-     },
-
-    {sku: "002",
-     category: "groceries",
-     name: "Carrot",
-     price: 5.50,
-     inventory: 12
-    },
-
-    {sku: "003",
-     category: "electronics",
-     name: "Robot",
-     price: 50.99,
-     inventory: 30
-    },
-
-    {sku: "004",
-     category: "household",
-     name: "Table",
-     price: 35.50,
-     inventory: 40
-    },
-
-    {sku: "005",
-     category: "apparel",
-     name: "Hair Ties",
-     price: 8.00,
-     inventory: 8
-    }
+    {name: "Willy Smith", hourlyRate: 500, hoursWorked: 40},
+    {name: "Sab Carpenter", hourlyRate: 600, hoursWorked: 35},
+    {name: "Addy Rae", hourlyRate: 200, hoursWorked: 15},
   ];
 
-console.log("Step 2. Products Without Discount:");
-products.forEach(obj => console.log(`${obj.sku} | ${obj.category} | ${obj.name} | \$${obj.price} | ${obj.inventory}`));
 
