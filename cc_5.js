df7544e3e0594c66081c50ec5ca7e931ca08a900
thecodingdilettante <forbesm@usf.edu>
2025-09-25 11:38:59 -0400

    Step 5 tax

--- cc_5.js
+++ cc_5.js
@@ -7,29 +7,20 @@ let employees = [
   ];
 
   //BASE PAY
-let workingTime = 45;
-if (workingTime <= 40) {
   function calculateBasePay(rate, hours) {
-  return (rate * hours);
-  }
-let total = calculateBasePay(500, 40);
-console.log(total);
+  return rate * hours;
 }
-  else {
-console.log(`This person has worked over 40 hours.`)
-};
 
 //OVERTIME
 
-let overtimePay = 42;
-if (workingTime > 40) {
   function calculateOvertimePay(rate, hours) {
-    
-  return (rate * 1.5);
-  }
-let total = calculateOvertimePay(5, 42);
-console.log(total);
-}
-  else {
-console.log(`This person has not worked overtime.`)
+    extraHours = hours - 40
+    return extraHours * (rate * 1.5);
+    }
+
+// TAX
+
+function calculateTaxes(grossPay) {
+  tax = 0.15 * grossPay
+  return grossPay - tax;
 }
