fee794e5ce411b7ae404c75fabb85d0d5784b12d
thecodingdilettante <forbesm@usf.edu>
2025-09-25 11:31:24 -0400

    Step 3/4 initial

--- cc_5.js
+++ cc_5.js
@@ -6,4 +6,30 @@ let employees = [
     {name: "Addy Rae", hourlyRate: 200, hoursWorked: 15},
   ];
 
+  //BASE PAY
+let workingTime = 45;
+if (workingTime <= 40) {
+  function calculateBasePay(rate, hours) {
+  return (rate * hours);
+  }
+let total = calculateBasePay(500, 40);
+console.log(total);
+}
+  else {
+console.log(`This person has worked over 40 hours.`)
+};
 
+//OVERTIME
+
+let overtimePay = 42;
+if (workingTime > 40) {
+  function calculateOvertimePay(rate, hours) {
+    
+  return (rate * 1.5);
+  }
+let total = calculateOvertimePay(5, 42);
+console.log(total);
+}
+  else {
+console.log(`This person has not worked overtime.`)
+}
